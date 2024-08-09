c6335b277fe18bb0a8545ada4f733770ee7798fd
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-08-09 16:24:21 +0200

    header 10%

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4">
+        Header
+    </header>
+  )
+}
+
+export default Header

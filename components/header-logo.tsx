5d1093a9df2d1abc15752a8a8c6a74a79b139151
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-08-22 16:36:48 +0200

    finish header logo

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,7 +7,14 @@ type Props = {}
 function HeaderLogo({}: Props) {
   return (
     <>
-      <Link href={""} />
+      <Link href={"/"} >
+        <div className="items-center hidden lg:flex">
+          <Image src="/logo.svg" alt='Logo' height={28} width={28}/>
+          <p className="font-semibold text-white text-2xl ml-2.5">
+            Finance
+          </p>
+        </div>
+      </Link>
     </>
   )
 }
